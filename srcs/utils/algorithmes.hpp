659a08baddfc9461325c1b49013b410baf3670bb
clorin <cyrille.lorin@yahoo.fr>
2021-11-10 22:44:34 +0100

    ADD: in algorythme equal(predicate2)

--- srcs/utils/algorithmes.hpp
+++ srcs/utils/algorithmes.hpp
@@ -27,19 +27,31 @@ namespace ft
   		return (first2!=last2);
 	}
 
-    template <class T, class U>
-        bool equal(T first1, T last1, U first2)
+    template <class InputIterator1, class InputIterator2>
+        bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
         {
             while(first1 != last1)
             {
                 if(!(*first1 == *first2))
                     return (false);
-                    first1++;
-                    first2++;
+                first1++;
+                first2++;
             }
             return true;
         }
 
-    // TO DO equal(T first1, T last1, U first2, function)
+    template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+        bool equal(InputIterator1 first1, InputIterator1 last1,
+                   InputIterator2 first2, BinaryPredicate pred)
+        {
+            while(first1 != last1)
+            {
+                if(!pred(*first1, *first2))
+                    return (false);
+                first1++;
+                first2++;
+            }
+            return true;
+        }
 }
 #endif
