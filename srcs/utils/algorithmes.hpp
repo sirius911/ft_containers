a7a3621c6758d7d31cb8248d460c73f04906ff4e
Cyrille LORIN <clorin@e3r13p4.42.fr>
2021-11-12 13:57:33 +0100

    ADD:in algorithmes lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)

--- srcs/utils/algorithmes.hpp
+++ srcs/utils/algorithmes.hpp
@@ -6,12 +6,21 @@
 /*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 08:43:29 by clorin            #+#    #+#             */
-/*   Updated: 2021/11/12 11:26:11 by clorin           ###   ########.fr       */
+/*   Updated: 2021/11/12 13:52:03 by clorin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ALGORITHMES_HPP
 # define ALGORITHMES_HPP
+
+/*
+	** LEXICOGRAPHICAL_COMPARE
+	** Returns true if the range [first1,last1) compares lexicographically less than the range [first2,last2).
+	** A lexicographical comparison is the kind of comparison generally used to sort words alphabetically in dictionaries;
+	** It involves comparing sequentially the elements that have the same position in both ranges against each other until one element is not equivalent to the other.
+	** The result of comparing these first non-matching elements is the result of the lexicographical comparison.
+	** If both sequences compare equal until one of them ends, the shorter sequence is lexicographically less than the longer one.
+	*/
 
 namespace ft
 {
@@ -21,12 +30,27 @@ namespace ft
 	{
   		while (first1!=last1)
  		{
-    		if (first2==last2 || *first2<*first1) return false;
-    		else if (*first1<*first2) return true;
+    		if (first2 ==last2 || *first2 < *first1) return false;
+    		else if (*first1 < *first2) return true;
     		++first1; ++first2;
   		}
-  		return (first2!=last2);
+  		return (first2 !=last2);
 	}
+
+    template <class InputIterator1, class InputIterator2, class Compare>
+        bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+        {
+            while ((first1 != last1) && (first2 != last2))
+		    {
+			    if (comp(*first1, *first2))
+				    return (true);
+			    if (comp(*first2, *first1))
+				    return (false);
+			    ++first1;
+			    ++first2;
+		    }
+		    return ((first1 == last1) && (first2 != last2));           
+        }
 
     template <class InputIterator1, class InputIterator2>
         bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
