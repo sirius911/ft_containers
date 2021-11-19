ef24f7c22233bd69f5d69ec141d7a6dfeef39200
clorin <cyrille.lorin@yahoo.fr>
2021-11-19 12:22:55 +0100

    ADD: operator ++ and --

--- srcs/bidirectional_iterator.hpp
+++ srcs/bidirectional_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 08:12:02 by clorin            #+#    #+#             */
-/*   Updated: 2021/11/19 09:18:11 by clorin           ###   ########.fr       */
+/*   Updated: 2021/11/19 12:15:19 by clorin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,11 @@ namespace ft
         public:
             //constructors
             bidirectional_iterator():_ptr(nullptr), _root(nullptr), _nill(nullptr){}    //default
-            bidirectional_iterator(node_ptr ptr, node_ptr root, node_ptr nill):_ptr(ptr), _root(root), _nill(nill){}
             bidirectional_iterator(const bidirectional_iterator &cpy):_ptr(cpy._ptr), _root(cpy._root), _nill(cpy._nill){} //copy
             virtual ~bidirectional_iterator(){} //destructor
 
+            bidirectional_iterator(node_ptr ptr, node_ptr root, node_ptr nill):_ptr(ptr), _root(root), _nill(nill){}
+            
             bidirectional_iterator &operator=(bidirectional_iterator const &cpy)
             {
                 if (this !=&cpy)
@@ -58,6 +59,88 @@ namespace ft
                 }
                 return *this;
             }
+
+            /*      Access      */
+            reference       operator*()      { return this->_ptr->data;}
+            const_reference operator*() const { return _ptr->data;}
+
+            pointer         operator->()       {return &(operator*());}
+            const_pointer   operator->() const {return &(operator*());}
+
+            /*increment */
+
+            bidirectional_iterator  &operator++(void)
+            {
+                if (_ptr != _nill)
+                _ptr = successor(_ptr);
+                return *this;
+            }
+            bidirectional_iterator  operator++(int)
+            {
+                bidirectional_iterator  old(*this);
+                this->operator++();
+                return old;
+            }
+
+            /*  decrement   */
+            bidirectional_iterator  &operator--(void)
+            {
+                if(_ptr == _nill)
+                    _ptr = max(_root);
+                else
+                    _ptr = predecessor(_ptr);
+                return *this;
+            }
+
+            bidirectional_iterator  operator--(int)
+            {
+                bidirectional_iterator  old(*this);
+                this->operator--();
+                return old;
+;            }
+
+            private:
+                node_ptr    successor(node_ptr node)
+                {
+                    if(node->right != _nill)        // if there is a right children 
+                        return min(node->right);    // the successor is the minimun of right childern
+                    node_ptr    y = node->parent;
+                    while(y != NULL && node == y->right)
+                    {
+                        node = y;
+                        y = y->parent;
+                    }
+                    if (y == NULL)
+                        return _nill;
+                    return y;
+                }
+
+                node_ptr    predecessor(node_ptr node)
+                {
+                    if(node->left != _nill)         //if ther is a left children
+                        return (max(node->left));   // predecessor is th maximun of left children
+                    node_ptr    y = node->parent;
+                    while(y != NULL && node == y->left)
+                    {
+                        node = y;
+                        y = y->parent;
+                    }
+                    return y;
+                }
+
+                node_ptr    min(node_ptr node)
+                {
+                    while(node->left != _nill)
+                        node = node->left;
+                    return (node);
+                }
+
+                node_ptr    max(node_ptr node)
+                {
+                    while(node->right != _nill)
+                        node = node->right;
+                    return node;
+                }
     };
 }//ft
 #endif
